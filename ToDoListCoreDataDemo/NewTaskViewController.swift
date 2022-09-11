318cb5aff77612705a30fa36d2202ce2a1935a70
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-11 20:43:35 +0300

    add cancelButton and set its constraints

--- ToDoListCoreDataDemo/NewTaskViewController.swift
+++ ToDoListCoreDataDemo/NewTaskViewController.swift
@@ -33,6 +33,17 @@ class NewTaskViewController: UIViewController {
         button.addTarget(self, action: #selector(save), for: .touchUpInside)
         return button
     }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +54,8 @@ class NewTaskViewController: UIViewController {
     
     private func setupSubviews() {
         view.addSubview(taskTextField)
+        view.addSubview(saveButton)
+        view.addSubview(cancelButton)
     }
     
     private func setConstraints() {
@@ -56,9 +69,35 @@ class NewTaskViewController: UIViewController {
             taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor,
                                                     constant: -40)
         ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor,
+                                            constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                                constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                 constant: -40)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor,
+                                              constant: 20),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+                                                  constant: 40),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+                                                   constant: -40)
+        ])
     }
     
     @objc private func save() {
-        
+        dismiss(animated: true)
+    }
+    
+    @objc private func cancel() {
+        dismiss(animated: true)
     }
 }
