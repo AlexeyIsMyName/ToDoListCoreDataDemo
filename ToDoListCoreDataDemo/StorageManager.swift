d68e7fb649f8eedfb98ed59c53042974271e5aa4
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-09-13 15:09:59 +0300

    create StorageManager

---
+++ ToDoListCoreDataDemo/StorageManager.swift
@@ -0,0 +1,13 @@
+//
+//  StorageManager.swift
+//  ToDoListCoreDataDemo
+//
+//  Created by ALEKSEY SUSLOV on 13.09.2022.
+//
+
+import Foundation
+import CoreData
+
+class StorageManager {
+    
+}
